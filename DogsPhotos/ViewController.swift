54866ecad97f1e9eefe0e15aed48f48bff7e0774
MacOS <MacOS@MacOSs-MacBook-Air.local>
2018-09-19 05:21:35 +0300

    Parsed an array of photo urls from JSON

--- DogsPhotos/ViewController.swift
+++ DogsPhotos/ViewController.swift
@@ -12,14 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let arrayOfPhotos = parseFromJson()
+        print(arrayOfPhotos)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Parsing photo urls from JSON
+    
+    func parseFromJson() -> [String] {
+        guard let path = Bundle.main.path(forResource: "dog_urls", ofType: "json") else {
+            return [""]
+        }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            
+            guard let array = json as? [String: Array<String>] else { return [""] }
+            
+            guard let photos = array["urls"] else { return [""] }
+            return photos
+        }
+        catch {
+            print(error)
+        }
+        return [""]
+    }
+    
 }
 
